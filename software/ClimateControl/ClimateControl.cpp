b47dcd4aee34442f6ace0741f6ce430b134c7270
Pascal Weyprecht <fx@fx-world.de>
2016-12-09 15:48:10 +0100

    added logging to file

--- software/ClimateControl/ClimateControl.cpp
+++ software/ClimateControl/ClimateControl.cpp
@@ -27,7 +27,7 @@ void showDate(const DateTime& date) {
 	Serial.println();
 }
 
-bool isVentialationNeeded(float humidityInside, float humidityOutside) {
+bool isVentilationNeeded(float humidityInside, float humidityOutside) {
 	return (humidityInside > humidityOutside);
 }
 
@@ -50,25 +50,50 @@ void setup() {
 	Wire.begin();
 
 	if (rtc.lostPower()) {
-	    Serial.println("RTC lost power, reseting!");
+	    Serial.println("RTC lost power, resetting!");
 	    // following line sets the RTC to the date & time this sketch was compiled
 	    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 	}
 }
 
+/**
+ * Convert relative humidity to absolute humidity.
+ *
+ * see: https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
+ */
+float asAbsolute(float relativeHumidity, float temperature) {
+	return (6.112 * pow(EULER, ((17.67 * temperature)/(243.5 + temperature))) * relativeHumidity * 2.1674) / (273.15 + temperature);
+}
+
 void loop() {
-	float    humidityInside  = dht_inside.readHumidity();
-	float    humidityOutside = dht_outside.readHumidity();
-	DateTime now             = rtc.now();
+	// read sensor data
+	DateTime now                = rtc.now();
 
-	showDate(now);
+	float    humidityInside     = dht_inside.readHumidity();
+	float	 temperatureInside  = dht_inside.readTemperature();
+	float    humidityOutside    = dht_outside.readHumidity();
+	float	 temperatureOutside = dht_inside.readTemperature();
 
-	// TODO add delay for activation and deactivation, so it doesn't always turns off and on
-	if (isVentialationNeeded(humidityInside, humidityOutside)) {
-		setVentilation(true);
-	} else {
-		setVentilation(false);
-	}
+	bool     ventilationNeeded  = isVentilationNeeded(asAbsolute(humidityInside, temperatureInside), asAbsolute(humidityOutside, temperatureOutside));
 
+	// trigger ventilation and log to serial
+	setVentilation(ventilationNeeded);
+
+	// timestamp;humid-out;temp-out;humid-in;temp-in;vent-set
+	char* logline = (char*) malloc(128 * sizeof(char));
+	sprintf(logline, "%d-%d-%dT%d:%d:%d;%d;%d;%d;%d;%d", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second(),
+			humidityInside, temperatureInside, humidityOutside, temperatureOutside, ventilationNeeded);
+
+	// append logline to logfile for current date
+	char* filename = (char*) malloc(14 * sizeof(char));
+	sprintf(filename, "%d-%d-%d.txt", now.year(), now.month(), now.day());
+
+	File file = SD.open(filename, O_APPEND);
+	file.write(logline);
+	file.close();
+
+	Serial.println(logline);
+
+	// wait for sensors to get measure new values
 	delay(10000);
 }
